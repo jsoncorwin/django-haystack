adbb72c79e4fbeef938b1b6f55e1bbb064a11f9b
Jason Corwin <jsoncorwin@gmail.com>
2012-12-25 18:06:21 -0800

    allow uuid as PK in solr

--- haystack/utils/__init__.py
+++ haystack/utils/__init__.py
@@ -3,7 +3,7 @@ from haystack.constants import ID, DJANGO_CT, DJANGO_ID
 from haystack.utils.highlighting import Highlighter
 
 
-IDENTIFIER_REGEX = re.compile('^[\w\d_]+\.[\w\d_]+\.\d+$')
+IDENTIFIER_REGEX = re.compile('^[\w\d_]+\.[\w\d_]+\.[\d\w]+$')
 
 
 def get_model_ct(model):
@@ -14,20 +14,20 @@ def get_identifier(obj_or_string):
     """
     Get an unique identifier for the object or a string representing the
     object.
-    
+
     If not overridden, uses <app_label>.<object_name>.<pk>.
     """
     if isinstance(obj_or_string, basestring):
         if not IDENTIFIER_REGEX.match(obj_or_string):
             raise AttributeError("Provided string '%s' is not a valid identifier." % obj_or_string)
-        
+
         return obj_or_string
-    
+
     return u"%s.%s.%s" % (obj_or_string._meta.app_label, obj_or_string._meta.module_name, obj_or_string._get_pk_val())
 
 
 def get_facet_field_name(fieldname):
     if fieldname in [ID, DJANGO_ID, DJANGO_CT]:
         return fieldname
-    
+
     return "%s_exact" % fieldname
